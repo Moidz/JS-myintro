46b3ab09cc97558d91272877d2afd42f04ad05a3
Iyanuoluwa Odelotan <iyanuodelotan@gmail.com>
2022-06-21 06:13:47 +0100

    Displaying output

---
+++ script.js
@@ -0,0 +1,6 @@
+var myName = " Name: Odelotan Iyanuoluwa Dorcas";
+var myHeight = 'Height: 5ft.6';
+var myCountry = 'Country: Nigeria';
+document.getElementById("name").innerHTML = myName;
+document.getElementById("height").innerHTML = myHeight;
+document.getElementById("country").innerHTML = myCountry;
