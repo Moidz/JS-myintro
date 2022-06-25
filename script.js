255797e1c55e5061d0489153e90e198706bd8f33
Moidz <iyanuodelotan@gmail.com>
2022-06-25 05:46:37 +0100

    change variable declaration

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var myName = "My name is Odelotan Iyanuoluwa Dorcas";
-var myHeight = 'I am 5ft.6 tall';
-var myCountry = "My Country is Nigeria";
+let myName = "My name is Odelotan Iyanuoluwa Dorcas";
+let myHeight = "I am 5ft.6 tall";
+let myCountry = "My Country is Nigeria";
 alert(myName);
 alert(myHeight);
 alert(myCountry);
